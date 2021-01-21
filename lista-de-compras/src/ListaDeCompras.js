e9689e11a2f9795f57375c770b5bf62c5def05a9
beatrizodorcik <beaodorcik@gmail.com>
2021-01-20 21:54:25 -0300

    Exercício ListaDeCompras

---
+++ lista-de-compras/src/ListaDeCompras.js
@@ -0,0 +1,17 @@
+import Item from './Item'
+
+function ListaDeCompras() {
+  return (
+    <div className='app'>
+      <Item nome="Arroz" />
+      <Item nome="Feijão" />
+      <Item nome="Chocolate" />
+      <Item nome="Cenoura" />
+      <Item nome="Tomate" />
+    </div>
+  );
+}
+export default ListaDeCompras;
+
+
+
